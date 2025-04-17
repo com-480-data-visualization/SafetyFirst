338eb9f80d6ed8f938cbdc8782b5982eca25cd23
abdou-u <ahmed.abdelmalek@epfl.ch>
2025-04-17 18:25:54 +0200

    remove unused json f

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/SafetyFirst/', // <-- set this to the repo name
   plugins: [react()],
 })
